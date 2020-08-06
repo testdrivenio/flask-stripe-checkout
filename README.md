266eeabe36a48fd6678929a2c20fa96485b00a35
Michael Herman <hermanmu@gmail.com>
2020-08-06 07:17:11 -0500

    update to new stripe checkout

--- README.md
+++ README.md
@@ -1,3 +1,36 @@
-# Adding a Custom Stripe Checkout to a Flask App
+# Setting up Stripe Checkout with Flask
 
-Blog post: https://testdriven.io/blog/adding-a-custom-stripe-checkout-to-a-flask-app/
+## Want to learn how to build this?
+
+Check out the [post](https://testdriven.io/blog/flask-stripe-tutorial/).
+
+## Want to use this project?
+
+1. Fork/Clone
+
+1. Create and activate a virtual environment:
+
+    ```sh
+    $ python3 -m venv venv && source venv/bin/activate
+    ```
+
+1. Install the requirements:
+
+    ```sh
+    (venv)$ pip install -r requirements.txt
+    ```
+
+1. Add your Stripe test secret and publishable keys as environment variables like so:
+
+    ```sh
+    (venv)$ export STRIPE_PUBLISHABLE_KEY=<YOUR_STRIPE_PUBLISHABLE_KEY>
+    (venv)$ export STRIPE_SECRET_KEY=<YOUR_STRIPE_SECRET_KEY>
+    ```
+
+1. Run the server:
+
+    ```sh
+    (venv)$ FLASK_ENV=development python app.py
+    ```
+
+    Navigate to [http://localhost:5000](http://localhost:5000).
---
+++ app.py
@@ -0,0 +1,77 @@
+import os
+
+import stripe
+from flask import Flask, jsonify, render_template
+
+app = Flask(__name__)
+
+stripe_keys = {
+    "secret_key": os.environ["STRIPE_SECRET_KEY"],
+    "publishable_key": os.environ["STRIPE_PUBLISHABLE_KEY"],
+}
+
+stripe.api_key = stripe_keys["secret_key"]
+
+
+@app.route("/hello")
+def hello_world():
+    return jsonify("hello, world!")
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/config")
+def get_publishable_key():
+    stripe_config = {"publicKey": stripe_keys["publishable_key"]}
+    return jsonify(stripe_config)
+
+
+@app.route("/create-checkout-session")
+def create_checkout_session():
+    domain_url = "http://localhost:5000/"
+    stripe.api_key = stripe_keys["secret_key"]
+
+    try:
+        # Create new Checkout Session for the order
+        # Other optional params include:
+        # [billing_address_collection] - to display billing address details on the page
+        # [customer] - if you have an existing Stripe Customer ID
+        # [payment_intent_data] - lets capture the payment later
+        # [customer_email] - lets you prefill the email input in the form
+        # For full details see https:#stripe.com/docs/api/checkout/sessions/create
+
+        # ?session_id={CHECKOUT_SESSION_ID} means the redirect will have the session ID set as a query param
+        checkout_session = stripe.checkout.Session.create(
+            success_url=domain_url + "success?session_id={CHECKOUT_SESSION_ID}",
+            cancel_url=domain_url + "cancelled",
+            payment_method_types=["card"],
+            mode="payment",
+            line_items=[
+                {
+                    "name": "T-shirt",
+                    "quantity": 1,
+                    "currency": "usd",
+                    "amount": "2000",
+                }
+            ]
+        )
+        return jsonify({"sessionId": checkout_session["id"]})
+    except Exception as e:
+        return jsonify(error=str(e)), 403
+
+
+@app.route("/success")
+def success():
+    return render_template("success.html")
+
+
+@app.route("/cancelled")
+def cancelled():
+    return render_template("cancelled.html")
+
+
+if __name__ == "__main__":
+    app.run()
